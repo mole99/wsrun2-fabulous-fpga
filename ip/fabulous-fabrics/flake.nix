b677a8b26fffa9d94f2b8db23f0124c330b494d0
Leo Moser <leomoser99@gmail.com>
2026-06-16 15:37:30 +0200

    Update fabric
    Signed-off-by: Leo Moser <leomoser99@gmail.com>

--- ip/fabulous-fabrics/flake.nix
+++ ip/fabulous-fabrics/flake.nix
@@ -9,7 +9,7 @@
   };
 
   inputs = {
-    librelane_plugin_fabulous.url = "github:mole99/librelane_plugin_fabulous/1.11.0";
+    librelane_plugin_fabulous.url = "github:mole99/librelane_plugin_fabulous/1.12.0";
   };
 
   outputs =
